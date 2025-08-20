49316244241cdd3d603b3177fb7b2da74c36ed7a
Hoang Dung <hoangdunghtlc@gmail.com>
2025-08-20 20:33:40 +0700

    commit

--- ReactJS/Session_13/src/Components/Bai_06/DetailPost.tsx
+++ ReactJS/Session_13/src/Components/Bai_06/DetailPost.tsx
@@ -17,10 +17,10 @@ export default function DetailPost(props: Props) {
       <h2>Danh Sách Bài Viết</h2>
       {props.posts.map((post) => (
         <p key={post.id}>
-          <p>ID: {post.id}</p>
-          <p>Title: {post.title}</p>
-          <p>Content: {post.content}</p>
-          <p>Author: {post.author}</p>
+          <p><b>ID: {post.id}</b></p>
+          <p><b>Title: {post.title}</b></p>
+          <p><b>Content: {post.content}</b></p>
+          <p><b>Author: {post.author}</b></p>
           <hr />
         </p>
       ))}
