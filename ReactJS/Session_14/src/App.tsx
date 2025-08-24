bc7938e67a9a60d1866e9c7cc008dba76d261d99
Hoang Dung <hoangdunghtlc@gmail.com>
2025-08-24 22:56:00 +0700

    Session_15

--- ReactJS/Session_14/src/App.tsx
+++ ReactJS/Session_14/src/App.tsx
@@ -2,8 +2,8 @@
 // import Notification from '../Components/Notification';
 // import React from '../Components/React';
 // import React_Bai04 from '../Components/React_Bai04';
-// import Bai_05 from '../Components/Bai_05';
-import Bai_06 from '../Components/Bai_06';
+import Bai_05 from '../Components/Bai_05';
+// import Bai_06 from '../Components/Bai_06';
 
 function App() {
 
@@ -13,8 +13,8 @@ function App() {
       {/* <Notification /> */}
       {/* <React /> */}
       {/* <React_Bai04 /> */}
-      {/* <Bai_05 /> */}
-      <Bai_06 />
+      <Bai_05 />
+      {/* <Bai_06 /> */}
     </>
   )
 }
